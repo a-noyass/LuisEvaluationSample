78b56e3e4c0e77b93ed80b9c766adc57526824e9
NourEldin Yasser <noursalem95@gmail.com>
2021-01-19 11:12:57 +0200

    Basic working sample with no error or failure handling

---
+++ BatchTesting/BatchTestingRequest.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using Newtonsoft.Json;
+
+namespace BatchTesting
+{
+    class BatchTestingRequest
+    {
+        [JsonProperty("LabeledTestSetUtterances")]
+        public LabeledTestSetUtterance[] LabeledTestSetUtterances { get; set; }
+    }
+
+    public partial class LabeledTestSetUtterance
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("intent")]
+        public string Intent { get; set; }
+
+        [JsonProperty("entities")]
+        public EntityElement[] Entities { get; set; }
+    }
+
+    public partial class EntityElement
+    {
+        [JsonProperty("entity")]
+        public string Entity { get; set; }
+
+        [JsonProperty("startPos")]
+        public long StartPos { get; set; }
+
+        [JsonProperty("endPos")]
+        public long EndPos { get; set; }
+
+        [JsonProperty("children")]
+        public EntityElement[] Children { get; set; }
+    }
+
+}
---
+++ BatchTesting/LuisBatchTestClient.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace BatchTesting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+
+    internal class LuisBatchTestClient
+    {
+        private readonly string _key;
+
+        private readonly string _uri;
+
+        private static HttpClient _httpClient;
+
+        public LuisBatchTestClient(string endpoint, string appId, string key)
+        {
+            _key = key;
+            _uri = $"{endpoint}/luis/v3.0-preview/apps/{appId}/slots/production/evaluations";
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<OperationStatus> CreateEvaluationsOperationAsync(BatchTestingRequest batchInput)
+        {
+            var headers = new Dictionary<string, string>()
+            {
+                ["Ocp-Apim-Subscription-Key"] = _key
+            };
+            HttpResponseMessage res = await SendJsonPostRequestAsync(_uri, batchInput, headers);
+
+            if (res.StatusCode != HttpStatusCode.Accepted)
+            {
+                throw new Exception($"Received status code {res.StatusCode} and message {await res.Content.ReadAsStringAsync()}");
+            }
+            var responseString = await res.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<OperationStatus>(responseString);
+        }
+
+        public async Task<OperationStatus> GetEvaluationsStatusAsync(string operationId)
+        {
+            var url = _uri + $"/{operationId}/status";
+
+            var headers = new Dictionary<string, string>()
+            {
+                ["Ocp-Apim-Subscription-Key"] = _key
+            };
+
+            HttpResponseMessage res = await SendGetRequestAsync(url, headers);
+
+            if (res.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception($"Received status code {res.StatusCode} and message {await res.Content.ReadAsStringAsync()}");
+            }
+            var responseString = await res.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<OperationStatus>(responseString);
+        }
+
+        // TODO: handle pagination
+        public async Task<EvaluationResult> GetEvaluationsResultAsync(string operationId)
+        {
+            var url = _uri + $"/{operationId}/result?verbose=true";
+
+            var headers = new Dictionary<string, string>()
+            {
+                ["Ocp-Apim-Subscription-Key"] = _key
+            };
+
+            HttpResponseMessage res = await SendGetRequestAsync(url, headers);
+
+            if (res.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception($"Received status code {res.StatusCode} and message {await res.Content.ReadAsStringAsync()}");
+            }
+            var responseString = await res.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<EvaluationResult>(responseString);
+        }
+
+        private async Task<HttpResponseMessage> SendGetRequestAsync(string url, Dictionary<string, string> headers)
+        {
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Get, url))
+            {
+                PopulateRequestMessageHeaders(headers, requestMessage);
+                HttpResponseMessage response = await _httpClient.SendAsync(requestMessage);
+                return response;
+            }
+        }
+
+        private async Task<HttpResponseMessage> SendJsonPostRequestAsync(string url, object body, Dictionary<string, string> headers)
+        {
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Post, url))
+            {
+                PopulateRequestMessageHeaders(headers, requestMessage);
+                var requestBodyAsJson = JsonConvert.SerializeObject(body);
+                requestMessage.Content = new StringContent(requestBodyAsJson, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await _httpClient.SendAsync(requestMessage);
+                return response;
+            }
+        }
+
+        private void PopulateRequestMessageHeaders(Dictionary<string, string> headers, HttpRequestMessage requestMessage)
+        {
+            foreach (KeyValuePair<string, string> h in headers)
+            {
+                requestMessage.Headers.Add(h.Key, h.Value);
+            }
+        }
+    }
+}
